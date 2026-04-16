a4629e4614e4f30f8d65300d3bfa2a6e7c45e1a6
Hi 👋, I'm Mohammad Asim <muhammadasim4927@gmail.com>
2026-04-16 21:35:03 +0500

    chnages_in_modules_files

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 require("dotenv").config();
 const app = require("./src/app");
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
